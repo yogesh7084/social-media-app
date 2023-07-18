6bca2572e1eef7aebe51259813012493b109034e
Yogesh Shingte <shingteyogesh7541@gmail.com>
2023-07-18 13:46:15 +0530

    added code till postshare

--- client/src/components/profileSide/ProfileSide.jsx
+++ client/src/components/profileSide/ProfileSide.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch';
 import './ProfileSide.css';
 import ProfileCard from '../ProfileCard/ProfileCard';
+import FollowersCard from '../FollowersCard/FollowersCard';
 
 const ProfileSide = () => {
     return (
         <div className='ProfileSide'>
             <LogoSearch />
             <ProfileCard />
+            <FollowersCard />
         </div>
     )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/profileSide/ProfileSide'
+import PostSide from '../../components/PostSIde/PostSide'
 
 const Home = () => {
     return (
         <div className="home">
             {/* <div className="profileSide">Profile</div> */}
             <ProfileSide />
-            <div className="postSide">Post</div>
+            <PostSide />
             <div className="rightSide">RightSide</div>
         </div>
     )
