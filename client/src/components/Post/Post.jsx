6c8b66acaaf01874747cc690de00c940b3a86a8a
Yogesh Shingte <shingteyogesh7541@gmail.com>
2023-07-19 21:04:29 +0530

    added code of profile page , login page and signup page

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -16,7 +16,7 @@ const Post = ({ data }) => {
                 <img src={Share} alt="share-img" />
             </div>
 
-            <span>{data.likes} likes</span>
+            <span style={{color: "var(--gray)", fontSize: "12px"}}>{data.likes} likes</span>
 
             <div className="details">
                 <span><b>{data.name}</b></span>
--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -4,6 +4,8 @@ import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css'
 
 const ProfileCard = () => {
+
+    const ProfilePage = true;
     return (
         <div className='ProfileCard'>
             <div className="ProfileImages">
@@ -31,11 +33,23 @@ const ProfileCard = () => {
                         <span>Followers</span>
                     </div>
 
+                    {ProfilePage && (
+                        <>
+                            <div className="vl"></div>
+                            <div className="follow">
+                                <span>3</span>
+                                <span>Posts</span>
+                            </div>
+                        </>
+                    )}
+
                 </div>
                 <hr />
 
             </div>
-            <span>My Profile</span>
+            {!ProfilePage &&
+                <span>My Profile</span>
+            }
 
         </div>
     )
